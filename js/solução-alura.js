e9630b26242276c91719fa04b83f5afd463db5fc
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-05-13 21:43:08 -0300

    feat: solução alura lógica da função adicionar()

---
+++ js/solução-alura.js
@@ -0,0 +1,20 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById("nome-amigo");
+    let lista = document.getElementById("lista-amigos");
+
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent += ', ' + amigo.value;
+    }
+
+    amigo.value = '';    
+}
+
+function sortear() {
+  
+}
